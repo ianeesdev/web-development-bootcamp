d48181604c51c12163c4a5ebb8f99d76179f62a4
aneese421@gmail.com <aneese421@gmail.com>
2022-08-12 16:54:18 +0500

    bmi calculator and newsletter signup

---
+++ Web Development/APIs/Weather App/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const https = require("https");
+const bodyparser = require("body-parser");
+
+const app = express();
+app.use(bodyparser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+    const city = req.body.cityName;
+    const appid = "4ab0522233de8f480810483551aef095";
+    const unit = "metric";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appid}&units=${unit}`;
+    https.get(url, (response) => {
+        response.on("data", (data) => {
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+            res.write(`<p>The weather of ${weatherData.name} is currently ${weatherDescription}</p>`);
+            res.write("<h1>The temperature is " + temp + "</h1>");
+            res.write(`<img src="${iconUrl}" />`);
+            res.send();
+        })
+    })
+})
+
+app.listen(3000, () => {
+    console.log(`App listening at http://localhost:${3000}/`);
+});
