796cf6bbaefb8de0827a64a9d037c36cb7a968cf
aneese421@gmail.com <aneese421@gmail.com>
2022-08-13 13:48:54 +0500

    server setted

--- Web Development/APIs/Newsletter-Signup/app.js
+++ Web Development/APIs/Newsletter-Signup/app.js
@@ -53,6 +53,6 @@ app.post("/failure", (req, res) => {
     res.redirect("/");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`App listening at http://localhost:${3000}/`);
 });
