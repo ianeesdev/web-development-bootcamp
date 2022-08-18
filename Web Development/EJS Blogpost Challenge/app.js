344faede12e4f1b4e1deb74a9f824c4bd494fe1c
aneese421@gmail.com <aneese421@gmail.com>
2022-08-18 11:55:27 +0500

    integrate blogpost project with mongodb

--- Web Development/EJS Blogpost Challenge/app.js
+++ Web Development/EJS Blogpost Challenge/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require("ejs");
-const _ = require("lodash")
+const _ = require("lodash");
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -14,12 +14,20 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let postArray = [];
+mongoose.connect("mongodb://localhost:27017/blogDB");
+
+const postSchema = mongoose.Schema({
+    title: String,
+    content: String
+});
+
+const Post = mongoose.model("Post", postSchema);
 
 app.get("/", (req, res) => {
-    res.render("home", {
-        content: homeStartingContent,
-        posts: postArray
+    Post.find({}, (error, foundList) => {
+        if (!error) {
+            res.render("home", { content: homeStartingContent, posts: foundList });
+        }
     });
 })
 
@@ -36,18 +44,20 @@ app.get("/compose", (req, res) => {
 })
 
 app.post("/compose", (req, res) => {
-    const newPost = {
+    const newPost = new Post({
         title: req.body.postTitle,
-        body: req.body.postContent
-    }
-    postArray.push(newPost);
-    res.redirect("/");
-})
+        content: req.body.postContent
+    });
+    newPost.save((error) => {
+        if (!error) res.redirect("/");
+    });
+});
 
-app.get("/posts/:post", (req, res) => {
-    postArray.forEach((post) => {
-        if (_.lowerCase(post.title) == _.lowerCase(req.params.post)) {
-            res.render("post", {post: post});
+app.get("/posts/:postID", (req, res) => {
+    const postID = req.params.postID;
+    Post.findOne({_id: postID}, (error, foundPost) => {
+        if (!error) {
+            res.render("post", {post: foundPost});
         }
     })
 })
